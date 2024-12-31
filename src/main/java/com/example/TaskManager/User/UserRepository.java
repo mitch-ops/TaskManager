36aaed13582ae0e302652fbca515d6e08ed95411
Mitchell <mitch.heidbrink@gmail.com>
2024-12-30 19:14:26 -0500

    configure User Repository

--- src/main/java/com/example/TaskManager/User/UserRepository.java
+++ src/main/java/com/example/TaskManager/User/UserRepository.java
@@ -1,6 +1,13 @@
 package com.example.TaskManager.User;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends Repository {
+import java.util.Optional;
+import java.util.UUID;
+
+// JpaRepository extends CrudRepository, just comes with some more methods (pagination, sorting)
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
 }
